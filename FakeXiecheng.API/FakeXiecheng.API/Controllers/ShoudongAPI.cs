78a702b756b80921bc690a745876d542674f5402
yangdawen <1352559801@qq.com>
2021-04-26 22:53:37 +0800

    2-5 添加第一个API

---
+++ FakeXiecheng.API/FakeXiecheng.API/Controllers/ShoudongAPI.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FakeXiecheng.API.Controllers
+{
+    [Route("api/shoudongApi")]//设置API的路由
+    public class ShoudongAPI : Controller
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
--- FakeXiecheng.API/FakeXiecheng.API/Startup.cs
+++ FakeXiecheng.API/FakeXiecheng.API/Startup.cs
@@ -14,18 +14,10 @@ namespace FakeXiecheng.API
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,14 +30,16 @@ namespace FakeXiecheng.API
             app.UseRouting();
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/test", async context =>
-                {
-                    await context.Response.WriteAsync("Hello from test!");
-                });
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello world!");
-                });
+                //endpoints.MapGet("/test", async context =>
+                //{
+                //    throw new Exception("这是一个测试错误！");
+                //    //await context.Response.WriteAsync("Hello from test!");
+                //});
+                //endpoints.MapGet("/", async context =>
+                //{
+                //    await context.Response.WriteAsync("Hello world!");
+                //});
+                endpoints.MapControllers();
             });
         }
     }
