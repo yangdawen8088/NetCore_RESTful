d3ece53414a9ab1e10bdb4e330744ba4cd91582c
yangdawen <1352559801@qq.com>
2021-04-27 22:41:15 +0800

    3-8 创建数据库

--- FakeXiecheng.API/FakeXiecheng.API/Startup.cs
+++ FakeXiecheng.API/FakeXiecheng.API/Startup.cs
@@ -1,3 +1,4 @@
+using FakeXiecheng.API.Database;
 using FakeXiecheng.API.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -10,19 +11,32 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace FakeXiecheng.API
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddTransient<ITouristRouteRepository, MockTouristRouteRepository>();
+            //services.AddTransient<ITouristRouteRepository, MockTouristRouteRepository>();
+            services.AddTransient<ITouristRouteRepository, TouristRouteRepository>();
             //services.AddTransient:每一次请求都会创建要给数据仓库，请求结束后将释放这个创建的仓库
             //services.AddSingleton:所有服务请求时共用这一个，
             //services.AddScoped:介于以上两种方法之间,
+            services.AddDbContext<AppDbContext>(option=> {
+                //option.UseSqlServer(@"Data Source=(localdb)\ProjectsV13;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+                //option.UseSqlServer("");
+                option.UseSqlServer(Configuration["DbContext:ConnectionString"]);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
